d581f263907577d297e4c8b70063f20d54b52c27
Yongping Chen <yongping.chen@kii.com>
2015-10-27 15:41:38 +0900

    Sample code to login, register push in a simple page

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,52 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+ var APP_ID = "3c9f1460";
+ var APP_KEY = "4f928656a6ed37903644eb133245343e";
+ var kii;
+
+ var onLogin = function (){
+   var email = document.getElementById("email").value;
+   var password = document.getElementById("password").value;
+   kii.KiiUser.authenticate(email, password, {
+       success: function (theUser) {
+          // hide login table
+          document.getElementById("login_table").setAttribute('style','display:none;');
+          document.getElementById("loggedin_table").setAttribute('style','display:initial;');
+       },
+       failure: function (theUser, errorString) {
+         alert("Error authenticating: " + errorString);
+       }
+   });
+
+ };
+
+ var onSignup = function (){
+
+ };
+
+ var onPushRegister = function (){
+    window.kiiPush.register(kii, {
+       received: "pushReceived",
+       success: function (token) {
+           alert('token=' + token);
+       },
+       failure: function (msg) {
+           alert('error ' + msg);
+       }
+    });
+
+ };
+
+ var onLogout = function (){
+   kii.KiiUser.logOut();
+   // hide login table
+   document.getElementById("login_table").setAttribute('style','display:initial;');
+   document.getElementById("loggedin_table").setAttribute('style','display:none;');
+
+ };
+
+ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -37,15 +82,38 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        if(id === 'deviceready') {
+          document.getElementById("loginBtn").onclick = onLogin;
+          document.getElementById("pushRegisterBtn").onclick = onPushRegister;
+          document.getElementById("logoutBtn").onclick = onLogout;
+          kii = window.kii.create();
+          kii.Kii.initializeWithSite(APP_ID, APP_KEY, kii.KiiSite.JP);
 
-        console.log('Received Event: ' + id);
+          window.kiiPush.initAndroid("539283181603", "pushReceived", {
+              user: {
+                  ledColor: "#FFFF00FF",
+                  notificatonText: "user"
+              },
+              direct: {
+                  showInNotificationArea: true,
+                  ledColor: "#FFFFFFFF",
+                  notificatonTitle: "$.title",
+                  notificatonText: "$.msg"
+              },
+              success: function () {
+                  console.log('init done');
+              },
+              failure: function (msg) {
+                  console.log('error ' + msg);
+              }
+          });
+        }
     }
-};
+ };
 
-app.initialize();
+ function pushReceived(args) {
+    alert('push received');
+ }
+
+ app.initialize();
